81c8c6d5dfaae3c914ac6bb0ded5e76e00ae876a
Albin GUIGNABERT <albin.guignabert@niji.fr>
2018-06-05 18:22:03 +0200

    [Count line number][WIP] Add basic count of line numbers

--- script.js
+++ script.js
@@ -4,23 +4,61 @@ arrayOfLines = content.split("\n");
 
 document.getElementsByTagName("pre")[0].innerHTML = "";
 
+let initial = 0;
+let initialLength = 0;
+let edited = 0;
+let editedLength = 0;
+let maxLineNumbers = 0;
+
 arrayOfLines.forEach(function (line) {
-    if (line.startsWith('+', 0)) {
-        document.getElementsByTagName('pre')[0].innerHTML += '<span class="plus">' + line + '</span>\n';
+
+    if (line.startsWith('@@ ', 0)) {
+        let lineSplited = line.split(" ");
+        if (lineSplited[3] === '@@') {
+            initial = lineSplited[1].split(',');
+            initialLength = initial[1];
+            initial = initial[0].replace('-', '');
+
+            edited = lineSplited[2].split(',');
+            editedLength = edited[1];
+            edited = edited[0].replace('+', '');
+
+            if (parseInt(initial) + parseInt(initialLength) > parseInt(edited) + parseInt(editedLength)) {
+                maxLineNumbers = parseInt(initial) + parseInt(initialLength);
+            } else {
+                maxLineNumbers = parseInt(edited) + parseInt(editedLength);
+            }
+        }
     }
-    else if (line.startsWith('-', 0)) {
+
+    if (line.startsWith('+', 0)) {
+
+        document.getElementsByTagName('pre')[0].innerHTML += (initial++) + ' | ';
+        document.getElementsByTagName('pre')[0].innerHTML += ('  ') + ' | ';
+        document.getElementsByTagName('pre')[0].innerHTML += '<span class="plus">' + line + '</span>\n';
+
+    }
+    else if (line.startsWith('- ', 0)) {
+        document.getElementsByTagName('pre')[0].innerHTML += ('  ') + ' | ';
+        document.getElementsByTagName('pre')[0].innerHTML += (edited++) + ' | ';
         document.getElementsByTagName('pre')[0].innerHTML += '<span class="minus">' + line + '</span>\n';
+    }
+    else if (line.startsWith(' ', 0)) {
+
+        document.getElementsByTagName('pre')[0].innerHTML += (initial++) + ' | ';
+        document.getElementsByTagName('pre')[0].innerHTML += (edited++) + ' | ';
+        document.getElementsByTagName('pre')[0].innerHTML += line + '\n';
+
     }
     else if (line.startsWith('diff', 0)) {
 
         // Regex that match a/... and b/...
         regexAB = new RegExp("\^\([ab])\/\?");
 
-        lineSplited = line.split(" ");
+        let lineSplited = line.split(" ");
         lineSplited.forEach(function (word, index, arrayTest) {
-            resultat = regexAB.test(word);
             // Highlight filename
-            if (resultat === true) {
+            if (regexAB.test(word) === true) {
                 wordSplited = word.split('/');
                 wordSplited[wordSplited.length - 1] = '<span class="filename">' + wordSplited[wordSplited.length - 1] + '</span>';
                 arrayTest[index] = '<b>' + wordSplited.join('/') + '</b>';
