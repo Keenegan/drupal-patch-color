293ee0e938f65b40555f19aecf3dd00ee4ad3ab4
Albin GUIGNABERT <albin.guignabert@niji.fr>
2018-09-02 19:44:37 +0200

    Major refactor of code - Create Diff class

--- script.js
+++ script.js
@@ -4,64 +4,73 @@ let content = document.getElementsByTagName("pre")[0].innerHTML;
 
 document.getElementsByTagName("pre")[0].innerHTML = '';
 
-let arrayOfLines = content.split("\n");
+let lines = content.split("\n");
 
 let initial = 0;
 let initialLength = 0;
 let edited = 0;
 let editedLength = 0;
-let codeStart = 'false';
+
 let newLineDeleted = '';
 let newLineAdded = '';
 let newLineMeta = '';
 let linesNumberAdded = '';
 let linesNumberDeleted = '';
 
-arrayOfLines.forEach(function (line) {
+// [meta, oldCode, oldCodeLineNumber, newCodeLineNumber, newCode]
+let diffsArrays = [];
+
+class Diff {
+    constructor(meta) {
+        this.meta = meta;
+    }
+
+    print() {
+        console.log(this.meta);
+        console.log(this.oldCode);
+        console.log(this.newCode);
+        console.log(this.oldCodeLineNumber);
+        console.log(this.newCodeLineNumber);
+    }
+}
+
+lines.forEach(function (line) {
 
     if (line.startsWith('@@ ', 0)) {
-        let lineSplited = line.split(" ");
-        if (lineSplited[3] === '@@') {
-            codeStart = 'true';
-            initial = lineSplited[1].split(',');
-            initialLength = initial[1];
-            initial = initial[0].replace('-', '');
 
-            edited = lineSplited[2].split(',');
-            editedLength = edited[1];
-            edited = edited[0].replace('+', '');
+        fillCode();
 
-            newLineMeta += line + '</br>';
+        let lineSplited = line.split(' ');
 
-            if (newLineDeleted !== '') {
-                printCodeBlock();
-                newLineDeleted = '';
-                newLineAdded = '';
-                linesNumberAdded = '';
-                linesNumberDeleted = '';
-            }
+        newLineMeta += line + '</br>';
+        let diff = new Diff(newLineMeta);
+        diffsArrays.push(diff);
 
-            let textnode = document.createElement('blocMeta');
-            textnode.innerHTML = newLineMeta;
-            document.getElementsByTagName("pre")[0].appendChild(textnode);
-            newLineMeta = '';
-        }
+        newLineMeta = '';
+
+        initial = lineSplited[1].split(',');
+        initialLength = initial[1];
+        initial = initial[0].replace('-', '');
+
+        edited = lineSplited[2].split(',');
+        editedLength = edited[1];
+        edited = edited[0].replace('+', '');
+
     }
-
-    if (codeStart === 'true' && line.startsWith('+++ ', 0) || line.startsWith('--- ', 0)) {
+    else if (line.startsWith('+++ ', 0) || line.startsWith('--- ', 0)) {
         newLineMeta += line + '</br>';
     }
-    else if (codeStart === 'true' && line.startsWith('+', 0)) {
+    else if (line.startsWith('+', 0)) {
         newLineAdded += '<span class="line"><span class="plus">' + line + '</span></span>\n';
         linesNumberAdded += '<span>' + initial + '</span>';
         initial++;
     }
-    else if (codeStart === 'true' && line.startsWith('-', 0)) {
+    else if (line.startsWith('-', 0)) {
         newLineDeleted += '<span class="line"><span class="minus">' + line + '</span></span>\n';
         linesNumberDeleted += '<span>' + edited + '</span>';
         edited++;
     }
-    else if (codeStart === 'true' && line.startsWith(' ', 0)) {
+    else if (line.startsWith(' ', 0)) {
         linesNumberAdded += '<span>' + initial + '</span>';
         linesNumberDeleted += '<span>' + edited + '</span>';
         newLineAdded += '<span class="line"><span>' + line + '</span></span>\n';
@@ -74,11 +83,28 @@ arrayOfLines.forEach(function (line) {
         newLineMeta += line;
     }
     else {
-        newLineMeta += line + '\n';
+        newLineMeta += line + '</br>';
     }
 
 });
+
+fillCode();
 printCodeBlock();
+
+function fillCode () {
+    if (newLineAdded !== '' || newLineDeleted !== '') {
+        let diff = diffsArrays[diffsArrays.length - 1];
+        diff.oldCode = newLineDeleted;
+        diff.oldCodeLineNumber = linesNumberDeleted;
+        diff.newCodeLineNumber = linesNumberAdded;
+        diff.newCode = newLineAdded;
+
+        newLineDeleted = '';
+        linesNumberDeleted = '';
+        linesNumberAdded = '';
+        newLineAdded = '';
+    }
+}
 
 function colorFileName(line) {
     // Regex that match a/... and b/...
@@ -102,27 +128,40 @@ function colorFileName(line) {
 
 
 function printCodeBlock() {
-    let bloc1 = document.createElement('bloc');
-    let linesBloc = document.createElement('line-bloc');
-    let bloc2 = document.createElement('bloc');
-    let codeBloc = document.createElement('codeBloc');
-    let linesAdd = document.createElement('add');
-    let linesDel = document.createElement('delete');
 
-    bloc1.className = 'left-bloc';
-    bloc2.className = 'right-bloc';
-    bloc1.innerHTML = newLineDeleted;
-    bloc2.innerHTML = newLineAdded;
-    linesAdd.innerHTML = linesNumberAdded;
-    linesDel.innerHTML = linesNumberDeleted;
+    diffsArrays.forEach(function (diff) {
 
-    let pre = document.getElementsByTagName("pre")[0];
-    pre.appendChild(codeBloc);
-    codeBloc.appendChild(bloc1);
-    codeBloc.appendChild(linesBloc);
-    linesBloc.appendChild(linesDel);
-    linesBloc.appendChild(linesAdd);
-    codeBloc.appendChild(bloc2);
+        let blocMeta = document.createElement('blocMeta');
+        let bloc1 = document.createElement('bloc');
+        let bloc2 = document.createElement('bloc');
+        let linesBloc = document.createElement('line-bloc');
+
+        let codeBloc = document.createElement('codeBloc');
+        let linesAdd = document.createElement('add');
+        let linesDel = document.createElement('delete');
+
+        bloc1.className = 'left-bloc';
+        bloc2.className = 'right-bloc';
+        blocMeta.className = 'meta';
+
+        blocMeta.innerHTML = diff.meta;
+        bloc1.innerHTML = diff.oldCode;
+        bloc2.innerHTML = diff.newCode;
+        linesAdd.innerHTML = diff.newCodeLineNumber;
+        linesDel.innerHTML = diff.oldCodeLineNumber;
+
+        let pre = document.getElementsByTagName("pre")[0];
+        pre.appendChild(blocMeta);
+        pre.appendChild(codeBloc);
+
+        codeBloc.appendChild(bloc1);
+        codeBloc.appendChild(linesBloc);
+        linesBloc.appendChild(linesDel);
+        linesBloc.appendChild(linesAdd);
+        codeBloc.appendChild(bloc2);
+    });
+
+
 }
 
 // Bind scroll event to every code bloc
