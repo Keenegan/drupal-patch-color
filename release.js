b2a3e5c91710f6ad9390f9f62fd4b63637baa510
Albin GUIGNABERT <albin.guignabert@niji.fr>
2018-09-13 16:51:37 +0200

    [Automation] WIP Add script to increment version

---
+++ release.js
@@ -0,0 +1,11 @@
+//const argv = require('yargs');
+
+
+//if (argv.ships > 3 && argv.distance < 53.5) {
+//    console.log('Plunder more riffiwobbles!')
+//} else {
+//    console.log('Retreat from the xupptumblers!')
+//}
+
+
+
